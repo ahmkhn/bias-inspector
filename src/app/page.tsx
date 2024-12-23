04b0d1086ecf5fc24628379705775b09cdab94a2
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-12-22 22:20:14 -0500

    db fetch test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      Hello
+      {posts.map((post)=>(<div key={post.id}>{post.name}</div>))}
     </main>
   );
 }
