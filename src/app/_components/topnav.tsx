a841d251ceda1c4d7d828b2af1a8a7a39bec5435
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2025-01-07 20:15:01 -0500

    style: add favicon to topnav

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -3,13 +3,16 @@
 import { SignedOut, SignInButton, SignOutButton, SignedIn, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-
+import Image from "next/image";
 export function TopNav() {
   const router = useRouter();
 
   return (
     <nav className="sticky top-0 z-50 flex w-full items-center justify-between border-b bg-white p-4 backdrop-blur-md">
-      <Link href="/" className="text-xl font-semibold text-primary">Bias Inspector</Link>
+      <div className="flex flex-row items-center gap-2 cursor-pointer border-2 border-slate-500 rounded-md p-1" onClick={ ()=>router.push("/") }>
+        <Image src="/favicon.ico" alt="Bias Inspector" width={35} height={35}/>
+        <Link href="/" className="text-xl font-semibold text-primary">Bias Inspector</Link>
+      </div>
       <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton>
@@ -19,7 +22,7 @@ export function TopNav() {
           </SignInButton>
         </SignedOut>
         <SignedIn>
-          <UserButton afterSignOutUrl="/" />
+          <UserButton/>
         </SignedIn>
       </div>
     </nav>
